06dba76566e55b136cf2a06cdd6b1642a8aefb99
saniho <52693545+saniho@users.noreply.github.com>
2021-10-11 22:18:50 +0200

    Add files via upload

--- content-card-linky-editor.js
+++ content-card-linky-editor.js
@@ -92,11 +92,11 @@ export class contentCardLinkyEditor extends LitElement {
   get _showYesterdayRatio() {
     return this._config.showYesterdayRatio !== false;
   }
-  get _showTitreLigne() {
-    return this._config.showTitreLigne !== false;
-  }
   get _showError() {
     return this._config.showError !== false;
+  }
+  get _showTitreLigne() {
+    return this._config.showTitreLigne !== false;
   }
 
   get _title() {
@@ -162,7 +162,7 @@ export class contentCardLinkyEditor extends LitElement {
             ${this.renderSwitchOption("Show ratio semaine", this._showWeekRatio, "showWeekRatio")}
             ${this.renderSwitchOption("Show ratio hier", this._showYesterdayRatio, "showYesterdayRatio")}
             ${this.renderSwitchOption("Show titre ligne", this._showTitreLigne, "showTitreLigne")}
-            ${this.renderSwitchOption("Show erreur ligne", this._showError, "showError")}
+            ${this.renderSwitchOption("Show error", this._showError, "showError")}
           </ul>
           <!-- -->
           <paper-input
