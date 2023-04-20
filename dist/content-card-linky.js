7227031a512dd123c61fd7a9ceda6094ad2c8439
Arjan <44190435+vingerha@users.noreply.github.com>
2023-04-20 07:59:13 +0200

    Update content-card-linky.js

--- dist/content-card-linky.js
+++ dist/content-card-linky.js
@@ -352,7 +352,7 @@ class ContentCardLinky extends LitElement {
           ${this.renderDayPriceHCHP(dailyweek_costHP, dayNumber, config)}
           ${this.renderDayHCHP(dailyweek_HC, dayNumber, unit_of_measurement, config)}
           ${this.renderDayHCHP(dailyweek_HP, dayNumber, unit_of_measurement, config)}
-		  ${this.renderDayMaxPower(dailyweek_MP, dayNumber, dailyweek_MP_over, config)}
+		  ${this.renderDayMaxPower(dailyweek_MP_time, dayNumber, dailyweek_MP_over, config)}
         </div>
       `
   }
