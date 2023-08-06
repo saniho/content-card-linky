4b84e6a1184254d91ff8b53438bb8cb71608e078
Arjan <44190435+vingerha@users.noreply.github.com>
2023-08-06 13:19:42 +0200

    Update content-card-linky.js

--- dist/content-card-linky.js
+++ dist/content-card-linky.js
@@ -7,7 +7,7 @@ const css = LitElement.prototype.css;
 window.customCards = window.customCards || [];
 window.customCards.push({
   type: "content-card-linky",
-  name: "Carte Enedis12",
+  name: "Carte Enedis13",
   description: "Carte pour l'intégration myElectricalData.",
   preview: true,
   documentationURL: "https://github.com/saniho/content-card-linky",
